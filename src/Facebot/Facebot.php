f8eb8680ae900d9282b13a587466b14ec6d12922
Creedon <damian.creedon@musgravegroup.net>
2017-03-16 13:28:39 +0000

    Updated Facebot code to handle Facebook subscription request

--- src/Facebot/Facebot.php
+++ src/Facebot/Facebot.php
@@ -18,6 +18,16 @@ class Facebot
 	 * @var string
 	 */
 	protected $verify_token;
+	
+	/**
+	 * @var string
+	 */
+	protected $hub_verify_token;
+	
+	/**
+	 * @var string
+	 */
+	protected $challenge;
 
 
 	/**
@@ -29,9 +39,82 @@ class Facebot
 	{
 		$this->access_token = $access_token;
 		$this->verify_token = $verify_token;
+		$this->hub_verify_token = null;
+		$this->challenge = null;
 		
+	}
+	
+	public function requestType($query){
+		
+		//$query = $request->getQueryParams();
+		
+		$requestType = $query['hub_mode'];
+				
+		return $requestType;
+	}
+	
+	public function parseRequest($request){
+		
+		$query = $request->getQueryParams();
+		
+		//Subscription Request from FB
+		if (requestType($query) === 'subscribe' ){
+
+			setChallenge($query['hub_challenge']);
+			setHubVerifyToken($query['hub_verify_token']);		
+			$newResponse = $this->subscriptionResponse();			
+		}
+		
+		return $newResponse;
+		
+	}
+	
+	
+	public function checkToken($hub_verify_token){
+
+		if ($hub_verify_token === $this->$verify_token) {
+			$result = true;
+		}else{
+			$result = false;
+		}
+
+		return $result;
+	}
+		
+	public function subscriptionResponse(){
+		
+		if (checkToken(getHubVerifyToken())) {
+		
+			$this->logger->addInfo("Challenge: $challenge");
+			$body = $response->getBody();
+			$body->write($challenge);
+			$newResponse = $response->withBody($body);
+		
+			$newResponse = $response->withStatus(200);
+		}else{
+			$newResponse = $response->withStatus(500);
+		}
+		
+		
+		return $newResponse;
+	}
+	
+	
+	public function setHubVerifyToken($hub_verify_token){
+		$this->hub_verify_token = $hub_verify_token;
+		return $this;
+	}
+	
+	public function getHubVerifyToken(){
+		return $this->hub_verify_token;
+	}
+	
+	public function setChallenge($challenge){
+		$this->challenge = $challenge;
+		return $this;
 	}
 
 }
+
 
 
